42d860b0f7dbe1eb8856f75a348ae2eb49b53a21
Susan Minott <susan_minott@yahoo.com>
2022-08-19 22:27:48 -0400

    Event Handlers

--- main.js
+++ main.js
@@ -5,47 +5,51 @@ const posts = document.getElementById('posts')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(e)
-    
+    // console.log(e)
+    // console.log(e.target)
+    // console.log(e.target.value)
+    // console.log(e.target.post.value)
+    postsArea(e.target.post.value)
+    deleteBtn()
+    form.reset()
 })
 
-function postsArea(){
+function postsArea(comment){
     const ul = document.createElement('ul')
     const li = document.createElement('li')
     posts.appendChild(ul)
-    ul.appendChild(li)
+    // ul.appendChild(li)
     // console.log(posts)
     // li.innerText = 'I love that move'
-    li.innerHTML = `
-        <p>Hello world post 1</p>
+    console.log(comment)
+    ul.innerHTML = `
+        <li> 
+        <p>${comment}</p>
         <span class="options">
         <i class="fas fa-edit"></i>
         <i class="fas fa-trash-alt"></i>
         </span>
+        </li>
     `
+    // console.log(li.innerHTML)
 }
 postsArea()
 
+// DELETE COMMENT
+function deleteBtn(){
+    const deleteBtn = document.getElementsByClassName('fas fa-trash-alt')
+    // console.log(deleteBtn)
+    for(let btn of deleteBtn){
+        btn.addEventListener('click', (e) => {
+            e.target.parentNode.parentNode.remove()
+        })
+    }
+}
+deleteBtn()
 
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     // console.log(e);
-//     formValidation();  
-// })
-
-// const data = {}
-
-// const acceptData = () => {
-//     data["text"] = post.value;
-//     console.log(data);
-// }
-
-// const formValidation = () => {
-//     if (post.value === "") {
-//       // Other codes are here
-//     } else {
-//       // Other codes are here
-//       acceptData();
-//     }
-//   };
+// EDIT COMMENT
+function editBtn(){
+    const editBtn = document.getElementsByClassName('fas fa-edit')
+    // console.log(editBtn)
+}
+editBtn()
